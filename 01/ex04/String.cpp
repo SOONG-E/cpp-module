2e00794b874c7a7f692b378873a5209f2dc5d8b1
soong-e <sksuwjd@naver.com>
2022-12-07 19:47:27 +0900

    refact. delete unused variable

--- 01/ex04/String.cpp
+++ 01/ex04/String.cpp
@@ -6,7 +6,7 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 16:38:43 by yujelee           #+#    #+#             */
-/*   Updated: 2022/12/07 16:38:44 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2022/12/07 19:46:52 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ std::string String::getString(){
 
 void	String::replace(std::string s1, std::string s2){
 	size_t		idx;
-	int			len = _str.length();
 
 	idx = _str.find(s1);
 	while (idx != std::string::npos)
