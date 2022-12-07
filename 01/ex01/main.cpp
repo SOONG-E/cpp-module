51cbc99a8ad76a7821028c2a550f5e0c606e0828
soong-e <sksuwjd@naver.com>
2022-12-07 19:45:44 +0900

    feat. delete bornZombie

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -6,21 +6,14 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 15:57:00 by yujelee           #+#    #+#             */
-/*   Updated: 2022/12/07 10:37:16 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2022/12/07 19:45:19 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.h"
 
-void	bornZombie()
-{
-	Zombie *a = zombieHorde(10, "born");
-}
-
 int main(){
-	bornZombie();
 	Zombie *a = zombieHorde(10, "hey");
 
 	delete [] a;
-	
 }
