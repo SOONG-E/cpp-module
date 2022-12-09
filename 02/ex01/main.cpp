3cce492b22ad6339fc90f8c013e64eec287595dd
soong-e <sksuwjd@naver.com>
2022-12-09 17:25:45 +0900

    refact. remove unused header

--- 02/ex01/main.cpp
+++ 02/ex01/main.cpp
@@ -6,12 +6,11 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 21:32:21 by yujelee           #+#    #+#             */
-/*   Updated: 2022/12/09 15:17:51 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2022/12/09 17:24:24 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-#include <bitset>
 
 std::ostream& operator <<(std::ostream& outputStream, const Fixed& obj){
 	if ((obj.getRawBits() & 0xff) > 0){
