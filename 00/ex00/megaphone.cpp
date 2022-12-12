cac1b23059f1407ec35ba28ebfe2393710ab8341
soong-e <sksuwjd@naver.com>
2022-12-12 16:55:15 +0900

    refact. Remove else scope
    remove else scope for clean code

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 17:28:18 by yujelee           #+#    #+#             */
-/*   Updated: 2022/12/05 17:28:19 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2022/12/12 16:54:49 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 int main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
-	else
 	{
-		for (int i = 1; i < ac ; ++i)
-		{
-			std::string str(av[i]);
-			for (std::string::iterator it=str.begin(); it!=str.end(); ++it)
-				std::cout << ((char)std::toupper(*it));
-		}
-		std::cout<<std::endl;
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+		return (0);
 	}
+	for (int i = 1; i < ac ; ++i)
+	{
+		std::string str(av[i]);
+		for (std::string::iterator it=str.begin(); it!=str.end(); ++it)
+			std::cout << ((char)std::toupper(*it));
+	}
+	std::cout<<std::endl;
 	return (0);
 }
