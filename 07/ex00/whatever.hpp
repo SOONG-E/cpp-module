c5cb59aa8be9edef7e9d1524eba8b500a7f1fee6
soong-e <sksuwjd@naver.com>
2022-12-23 16:36:04 +0900

    feat. add ex00

---
+++ 07/ex00/whatever.hpp
@@ -0,0 +1,26 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <class T>
+void swap(T &a, T &b){
+	std::cout << "my swap" << std::endl;
+	T temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <class T>
+T min(T a, T b){
+	return (a > b ? b : a);
+}
+
+template <class T>
+T max(T a, T b){
+	return (a > b ? a : b);
+}
+
+#endif
