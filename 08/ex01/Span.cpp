e28096fc2503af29581a182cd88fa550e7822585
soong-e <sksuwjd@naver.com>
2023-01-05 17:17:25 +0900

    fix. remove addNumber()

--- 08/ex01/Span.cpp
+++ 08/ex01/Span.cpp
@@ -25,11 +25,7 @@ void Span::addNumber(int num){
 	_spots.push_back(num);
 }
 
-void Span::addNumber(std::vector<int> dummy){
-	std::transform(dummy.begin(), dummy.end(), std::back_inserter(_spots));
-
-	std::cout << _spots.size() << std::endl;
-}
+void Span::addNumber(std::vector<int> dummy){}
 
 int Span::shortestSpan(){
 	
