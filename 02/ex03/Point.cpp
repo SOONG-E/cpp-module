5f3066d0d7ddbe07b95d6e62a7de2a34f920b365
soong-e <sksuwjd@naver.com>
2023-01-11 15:02:04 +0900

    fix. rewrite assignment operator

--- 02/ex03/Point.cpp
+++ 02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 17:18:23 by yujelee           #+#    #+#             */
-/*   Updated: 2023/01/05 19:28:36 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2023/01/11 15:01:08 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,9 @@ Point::Point(const float x, const float y) :x(Fixed(x)), y(Fixed(y)){};
 Point::Point(const Point& obj) : x(obj.x), y(obj.y){}
 
 Point& Point::operator=(const Point& obj){
-	*this = Point(obj);
+	(void)obj;
+	std::cout << "assignment fail" << std::endl;
+	
 	return (*this);
 }
 
