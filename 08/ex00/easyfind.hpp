e84d88af8d963338d57e8cf89a0b90b2dc887ff7
soong-e <sksuwjd@naver.com>
2023-01-12 17:27:02 +0900

    feat. rewrite easyfind

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -2,14 +2,16 @@
 #define EASYFIND_HPP
 
 #include <vector>
+#include <list>
+#include <algorithm>
 
 template <typename T>
-int easyfind(T vec, int n){
-	for (typename T::size_type i = 0; i != vec.size() ; ++i){
-		if (vec[i] == n)
-			return (i);
-	}
-	return (-1);
+typename T::iterator easyfind(T vec, int n){
+	typename T::iterator result = std::find(vec.begin(), vec.end(), n);
+
+	if (result == vec.end())
+		throw std::invalid_argument("n isn't exist in vec");
+	return (result);
 }
 
 #endif
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -2,14 +2,47 @@
 #include <iostream>
 
 int main(){
-	std::vector<int> a;
-	a.push_back(1);
-	a.push_back(4);
-	a.push_back(3);
-	a.push_back(9);
-	a.push_back(23);
-	std::cout << easyfind(a, 3) <<std::endl;
-	std::cout << easyfind(a, 2) <<std::endl;
-
-	return (0);
+	{
+		try{
+			std::vector<int> a;
+			a.push_back(1);
+			a.push_back(4);
+			a.push_back(3);
+			a.push_back(9);
+			a.push_back(23);
+			std::cout << *easyfind(a, 3) <<std::endl;
+		}
+		catch (std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		try{
+			std::list<int> a;
+			a.push_back(1);
+			a.push_back(4);
+			a.push_back(3);
+			a.push_back(9);
+			a.push_back(23);
+			std::cout << *easyfind(a, 9) <<std::endl;
+		}
+		catch (std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		try{
+			std::list<int> a;
+			a.push_back(1);
+			a.push_back(4);
+			a.push_back(3);
+			a.push_back(9);
+			a.push_back(23);
+			std::cout << *easyfind(a, -1) <<std::endl;
+			std::cout << *easyfind(a, 7) <<std::endl;
+		}
+		catch (std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+	}
 }
